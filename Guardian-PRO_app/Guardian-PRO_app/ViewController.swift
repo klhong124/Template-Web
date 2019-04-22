1402dfecbf6d32a835c865836759e37d401ba157
Ryan Kwan <klhong124@gmail.com>
2019-04-22 11:57:05 +0800

    app_init

---
+++ Guardian-PRO_app/Guardian-PRO_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guardian-PRO_app
+//
+//  Created by Lok Hong Kwan on 22/4/2019.
+//  Copyright © 2019 Lok Hong Kwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
