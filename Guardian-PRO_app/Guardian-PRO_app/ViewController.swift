1bf947ae5bff8763c2b2f338bdce189deaed0418
Ryan Kwan <klhong124@gmail.com>
2019-04-27 12:52:40 +0800

    init

--- Guardian-PRO_app/Guardian-PRO_app/ViewController.swift
+++ Guardian-PRO_app/Guardian-PRO_app/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var unlock_icon: UIImageView!
+    @IBOutlet weak var lock_label: UILabel!
+    
+    func lock() {
+        let image = UIImage(named: "lock-icon")
+        self.unlock_icon.image = image
+    }
+    func unlock() {
+        let image = UIImage(named: "unlock-icon")
+        self.unlock_icon.image = image
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func onClick(_ lock_Button: UIButton, forEvent event: UIEvent){
+        
+        // Get current label text.
+        
+        let currLabelText = lock_label.text
+        
+        // Set new label text according to current text.
+        
+        if(currLabelText == "LOCK")
+        {
+            lock_label.text = "UNLOCK";
+            lock()
+        }else
+        {
+            lock_label.text = "LOCK";
+            unlock()
+        }
+        
+        // Make label size fit new text.
+        lock_label.sizeToFit();
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 }
-
