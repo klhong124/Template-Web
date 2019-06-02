88f0f04f3abd568ae06e61b4fe2352bf7d0980de
klhong124 <klhong124@gmail.com>
2019-06-02 22:40:08 +0800

    ok la

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
-var url = 'mongodb://localhost:27017/GuardianPRO-DB';
+var url = 'mongodb://192.168.1.81:27017/GuardianPRO-DB';
 
 MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
 	if (err) throw err;
